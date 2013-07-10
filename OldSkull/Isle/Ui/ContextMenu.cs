c285ec013351b0efa5ba2ea717c89e251a2a23c0
saint11 <saintjust@gmail.com>
2013-07-10 02:50:48 -0300

    tiles

--- OldSkull/Isle/Ui/ContextMenu.cs
+++ OldSkull/Isle/Ui/ContextMenu.cs
@@ -66,7 +66,7 @@ namespace OldSkull.Isle
 
         private void onPlace()
         {
-            player.PlaceItem();
+            player.InteractContainer(false);
             RemoveSelf();
         }
 
